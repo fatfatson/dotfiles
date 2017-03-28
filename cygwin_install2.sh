559eeddb99f98cbd1bb1f7b97eae6d7e8c08434a
wellbye <wellbye@163.com>
2017-03-28 21:48:45 +0800

    fix

--- cygwin_install2.sh
+++ cygwin_install2.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 apt-cyg install libevent-devel libncurses-devel make gcc-g++ vim curl tmux
 apt-cyg install luajit gdb bzip2 libzip2 libzip-devel libcurl-devel libjpeg-devel libpng-devel libfreetype-devel libiconv-devel php-iconv libmcrypt-devel ImageMagick
+apt-cyg install bash bash-completion
