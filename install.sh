5279432d69d0a038660ea921739e24167d92daf4
wellbye <wellbye@163.com>
2017-09-04 00:12:02 +0800

    fix

--- install.sh
+++ install.sh
@@ -18,5 +18,5 @@ rm -rf ~/.profile
 cat profile | sed s/\$USER/`whoami`/ > ~/.profile
 
 [ -f ~/.ssh/config ] && mv ~/.ssh/config  ~/dotfiles_old/sshconfig
-mkdir -p ~/.ssh/config
+mkdir -p ~/.ssh/
 ln -s $dir/sshconfig ~/.ssh/config
