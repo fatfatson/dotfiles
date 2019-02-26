d918423bb4506d398e5bc6078a2a510386411fea
wellbye <7355233@qq.com>
2019-02-26 16:07:53 +0800

    fix

--- mac_install_common.sh
+++ mac_install_common.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 brew install rename imagemagick ghostscript gnu-sed gnu-getopt\
     nmap mycli rlwrap wget bash git\
+    tmux reattach-to-user-namespace\
     bash-completion
